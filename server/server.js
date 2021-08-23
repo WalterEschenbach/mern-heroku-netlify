240f13e92ac2e7c316d3ee42350e21c15b19e1f9
Walter <eschenbach.walter@gmail.com>
2021-08-23 17:45:01 -0500

    install cors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
+// CORS Setup
+const corsOptions = {
+    origin: [process.env.DOMAIN_CLIENT || keys.domain.client, process.env.DOMAIN_SERVER || keys.domain.server],
+    credentials: true,
+}
+const cors = require('cors')
+app.use(cors(corsOptions))
+
+
 // Connect to MongoDB
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
