085d3356cafbf80e58b748449d1402dd18cb8d70
Walter <eschenbach.walter@gmail.com>
2021-08-24 09:50:15 -0500

    material ui style import to index.html and testing signinup on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-import axios from 'axios'
+//import SignIn from '../src/components/signIn/SignIn'
+import SignUp from '../src/components/signUp/SignUp'
+
+//import axios from 'axios'
 
 function App() {
 
-  const ping = () => {
-    axios({
-      method: 'get',
-      url: "https://mern-heroku-netlify-server.herokuapp.com/ping",
-      withCredentials: true
-    })
-      .then((response) => {
-        console.log('Response:', response)
-      })
-      .catch((error) => {
-        console.log('Error:', error)
-      })
-  }
+  // const ping = () => {
+  //   axios({
+  //     method: 'get',
+  //     url: "https://mern-heroku-netlify-server.herokuapp.com/ping",
+  //     withCredentials: true
+  //   })
+  //     .then((response) => {
+  //       console.log('Response:', response)
+  //     })
+  //     .catch((error) => {
+  //       console.log('Error:', error)
+  //     })
+  // }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div onClick={ping}>
-          Learn React
-        </div>
-      </header>
+      <SignUp />
     </div>
   );
 }
