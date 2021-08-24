c9aef46f0c72cd105e53b8af1c82fce55a8b22a3
Walter <eschenbach.walter@gmail.com>
2021-08-23 22:34:02 -0500

    updated axios ping request in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,8 @@ function App() {
   const ping = () => {
     axios({
       method: 'get',
-      url: 'http://localhost:3030',
-      withCredentials: true,
-      headers: {
-        "Access-Control-Allow-Origin": "http://localhost:3000/"
-      }
+      url: "https://mern-heroku-netlify-server.herokuapp.com/ping",
+      withCredentials: true
     })
       .then((response) => {
         console.log('Response:', response)
