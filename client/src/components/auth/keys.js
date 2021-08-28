c890778fc7b86c693d6337e10ade5faa86ce99c6
Walter <eschenbach.walter@gmail.com>
2021-08-28 15:24:08 -0500

    updated links for client requests

--- client/src/components/auth/keys.js
+++ client/src/components/auth/keys.js
@@ -9,8 +9,8 @@ module.exports = {
             client: "http://www.daydecider.com",
         },
         dev: {
-            server: "http://localhost:3030",
-            client: "http://localhost:3000",
+            server: "http://server.daydecider.com",
+            client: "http://www.daydecider.com",
         }
     }
 }
